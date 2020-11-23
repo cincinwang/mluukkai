26abd9ea3d07fae095958d88b0ed257f192b0abf
cincinwang <cincinwang0510@gmail.com>
2020-11-23 22:55:01 +0200

    step4

--- part1/src/index.js
+++ part1/src/index.js
@@ -3,19 +3,24 @@ import ReactDOM from 'react-dom';
 
 const App = () => {
     const course = 'Half Stack application development'
-    const part1 = {
-        name: 'Fundamentals of React',
-        exercise: 10
-    }
-    const part2 = {
+    const parts = [
+        {
+            name: 'Fundamentals of React',
+            exercise: 10
+        },
+        {
         name: 'Using props to pass data',
         exercise: 7
-    }
-    const part3 = {
+        },
+        {
         name: 'State of a component',
         exercise: 14
-    }
-
+        }
+     ];
+    const [part1, part2, part3] = parts;
+    console.log(part1.name);
+    console.log(part2.exercise);
+    console.log(part1);
     const Header = (props) => {
         return (
             <div><p>{props.course}</p></div>
