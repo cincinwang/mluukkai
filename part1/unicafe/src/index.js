7e90e71e6aba00567f915e811f7bcb0a2fb4b2e1
cincinwang <cincinwang0510@gmail.com>
2020-11-29 21:54:17 +0200

    step1

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,64 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-    const course = {
-        name: 'Half Stack application development',
-        parts: [
-            {
-                name: 'Fundamentals of React',
-                exercise: 10
-            },
-            {
-                name: 'Using props to pass data',
-                exercise: 7
-            },
-            {
-                name: 'State of a component',
-                exercise: 14
-            }
-        ]
-    };
-    const [part1, part2, part3] = course.parts;
-    console.log(part1.name);
-    console.log(part2.name);
-    console.log(part1);
 
-    const Header = () => {
-        return (
-            <div><p>{course.name}</p></div>
-        )
-    };
 
-    const Part = (props) => {
-        return (
-            <p>{props.part}{props.score}</p>
-        )
-    };
-    const Content = () => {
 
-        return(
-            <div>
-                <Part part={part1.name} score={part1.exercise} />
-                <Part part={part2.name} score={part2.exercise} />
-                <Part part={part3.name} score={part3.exercise} />
-            </div>
-        )
-    };
 
-    const Total = () => {
-        return(
-            <div><p>Number of exercise {part1.exercise + part2.exercise + part3.exercise}</p></div>
-        )
-    };
+const App = () =>{
+   const [good, setGood] = useState(0);
+   const [neutral, setNeutral] = useState(0);
+   const [bad, setBad] = useState(0)
 
-    return(
+    return (
         <div>
-            <Header course = {course} />
-            <Content parts= {course.parts}/>
-            <Total number = {course.parts} />
+            <p>give feedback</p>
+            <button>good</button>
+            <button>neutral</button>
+            <button>bad</button>
+            <p>statistics</p>
         </div>
     )
+
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
