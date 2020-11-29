637bfc267f137fad6abb13e17cf6a395aa9c7403
cincinwang <cincinwang0510@gmail.com>
2020-11-29 22:25:43 +0200

    step2

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,12 +1,15 @@
 import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 
+
 const App = () =>{
    const [good, setGood] = useState(0);
    const [neutral, setNeutral] = useState(0);
    const [bad, setBad] = useState(0);
 
-
+   const all = good +neutral + bad;
+   const average =  (good + bad*(-1))/all;
+   const positive = good/all;
     return (
         <div>
             <p>give feedback</p>
@@ -17,6 +20,9 @@ const App = () =>{
             <p>good {good}</p>
             <p>neutral {neutral}</p>
             <p>bad {bad}</p>
+            <p>All {all}</p>
+            <p>Average {average}</p>
+            <p>Positive {positive}</p>
         </div>
     )
 
