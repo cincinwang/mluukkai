15b80a21fc0d833f4f95a11d6f3108330f20c662
cincinwang <cincinwang0510@gmail.com>
2020-12-01 22:24:36 +0200

    step5

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -2,20 +2,18 @@ import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 
 const Statistics = (props) => {
-    if (props.good ===0 & props.neutral ===0 & props.bad ===0){
-        return(
-            <div>Statistic
-                <p>No feedback has been given</p>
-            </div>
-        )
-    }
     return(
-        <div>Statistics
-          <p>all {props.all}</p>
-          <p>average {props.average}</p>
-          <p>positive {props.positive}</p>
+        <div>
+          <p>{props.text} {props.value}</p>
         </div>
+    )
+};
 
+const Button = ({ onClick, text }) => {
+    return(
+        <button onClick={onClick}>
+          {text}
+        </button>
     )
 };
 
@@ -23,24 +21,60 @@ const App = () =>{
    const [good, setGood] = useState(0);
    const [neutral, setNeutral] = useState(0);
    const [bad, setBad] = useState(0);
+   const [allClicks, setAll] = useState([]);
 
-   const all = good +neutral + bad;
+   const all = good + neutral + bad;
    const average =  (good + bad*(-1))/all;
    const positive = good/all;
+
+   const handleGoodClick = () => {
+       setAll(allClicks.concat('G'));
+       setGood(good + 1)
+   };
+
+   const handleNeutralClick = () => {
+       setAll(allClicks.concat('N'));
+       setNeutral(neutral + 1)
+   };
+
+    const handleBadClick = () => {
+        setAll(allClicks.concat('B'));
+        setBad(bad + 1)
+    };
+
+    if (allClicks.length === 0){
+        return(
+            <div>
+                <p>give feedback</p>
+                <Button onClick={handleGoodClick} text="Good" />
+                <Button onClick={handleNeutralClick} text="Neutral" />
+                <Button onClick={handleBadClick} text="Bad" />
+                <p>{good}</p>
+                <p>{neutral}</p>
+                <p>{bad}</p>
+                <p>No feedback has been given</p>
+            </div>
+        )
+    }
+
     return (
         <div>
             <p>give feedback</p>
-            <button onClick={() => setGood(good +1)}>good</button>
-            <button onClick={() => setNeutral(neutral +1)}>neutral</button>
-            <button onClick={() => setBad(bad+1)}>bad</button>
+            <Button onClick={handleGoodClick} text="Good" />
+            <Button onClick={handleNeutralClick} text="Neutral" />
+            <Button onClick={handleBadClick} text="Bad" />
             <p>{good}</p>
             <p>{neutral}</p>
             <p>{bad}</p>
-            <Statistics good = {good} neutral={neutral} bad = {bad} all={all} average={average} positive={positive} />
-
+            <p>Statistics</p>
+            <Statistics text="good" value={good} />
+            <Statistics text="neutral" value={neutral} />
+            <Statistics text="bad" value={bad} />
+            <Statistics text="all" value={all} />
+            <Statistics text="average" value={average} />
+            <Statistics text="positive" value={positive} />
         </div>
     )
-
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
